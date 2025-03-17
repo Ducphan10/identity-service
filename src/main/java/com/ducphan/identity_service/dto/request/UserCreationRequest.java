a5093a8ef47a0f8dd2c3b1587ad850a48c1f1a3c
Ducphan10 <phanmanhduc10@gmail.com>
2025-03-18 02:01:58 +0700

    basic-exception-handling-and-validation

--- src/main/java/com/ducphan/identity_service/dto/request/UserCreationRequest.java
+++ src/main/java/com/ducphan/identity_service/dto/request/UserCreationRequest.java
@@ -1,5 +1,6 @@
 package com.ducphan.identity_service.dto.request;
 
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,7 +9,9 @@ import java.time.LocalDate;
 @Getter
 @Setter
 public class UserCreationRequest {
+    @Size(min = 3,message = "username must be atlest 3 characters")
     private String username;
+    @Size(min = 8, message = "Password must be atlest 8 characters")
     private String password;
     private String firstName;
     private String lastName;
